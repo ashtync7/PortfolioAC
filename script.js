7466a6e7fd500c0206cdf9d1ce57fe96b1470664
Ashtyn Ce <ashtyncza@gmail.com>
2020-08-28 10:30:24 -0400

    almost

--- script.js
+++ script.js
@@ -2,10 +2,9 @@ axios.get("https://ironrest.herokuapp.com/mia-aug2020-webdevs").then(res => {
    console.log(res)
 })
 
-axios.get("https://www.potterapi.com/v1/characters?key=$2a$10$AkgCGEKIUDVqXQ.rjYiMgu3kTgjm4QVSxU9zz188JXV7VPWSZaiOW&house=Gryffindor").then(res => {
-console.log(res)
-console.log(res.data)
-console.log(res.data[0])
-console.log(res.data[0].name)
-document.querySelector("#harry").innerText=res.data[0].name
+let btn=document.querySelector(".button2")
+btn.addEventListener("click", function(){
+    axios.get("https://www.potterapi.com/v1/characters?key=$2a$10$AkgCGEKIUDVqXQ.rjYiMgu3kTgjm4QVSxU9zz188JXV7VPWSZaiOW").then(res => {
+        document.querySelector("#HP").innerText=res.data[Math.floor(Math.random() * res.data.length)].name
+        }) 
 })
